2e794c203a8f7c1ceb8f37e0d82dae130f7d04b9
André Sousa <andre_sbatista@outlook.com>
2023-05-29 17:07:19 -0300

    Commit dos arquivos

---
+++ src/main/java/med/voll/api/medico/DadosAtualizacaoMedico.java
@@ -0,0 +1,13 @@
+package med.voll.api.medico;
+
+import jakarta.validation.constraints.NotNull;
+import med.voll.api.endereco.DadosEndereco;
+
+public record DadosAtualizacaoMedico(
+        @NotNull
+        Long id,
+        String nome,
+        String telefone,
+        DadosEndereco endereco) {
+
+}
---
+++ src/main/java/med/voll/api/medico/Especialidade.java
@@ -0,0 +1,20 @@
+package med.voll.api.medico;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+public enum Especialidade {
+    ORTOPEDIA,
+    CARDIOLOGIA,
+    GINECOLOGIA,
+    DERMATOLOGIA;
+
+    @JsonCreator
+    public static Especialidade fromString(String value) {
+        for (Especialidade especialidade : Especialidade.values()) {
+            if (especialidade.name().equalsIgnoreCase(value)) {
+                return especialidade;
+            }
+        }
+        throw new IllegalArgumentException("Especialidade inválida: " + value);
+    }
+}
